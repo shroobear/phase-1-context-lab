f76b87544b452735f5e0606de65a742d500be896
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-05-01 02:48:37 -0400

    all tests complete

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
 /* Your Code Here */
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+};
+
+let createEmployeeRecords= function(arr) {
+    return arr.map(createEmployeeRecord);
+};
+
+let createTimeInEvent = function(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+};
+
+let createTimeOutEvent = function(dateStamp) {
+    const [date, hour] = dateStamp.split(" ")
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(soughtDate) {
+    let inEvent = this.timeInEvents.find(function(e) {
+        return e.date === soughtDate
+    })
+    let outEvent = this.timeOutEvents.find(function(e) {
+        return e.date === soughtDate
+    })
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +66,19 @@ const allWagesFor = function () {
     return payable
 }
 
+const wagesEarnedOnDate = function (soughtDate) {
+    let payOwed = hoursWorkedOnDate.call(this, soughtDate) * this.payPerHour
+    return parseFloat(payOwed.toString())
+}
+
+const findEmployeeByFirstName = function (srcArray, firstName) {
+    return srcArray.find(function(rec) {
+        return rec.firstName === firstName
+    })
+}
+
+let calculatePayroll = function(arrayofEmployeeRecords) {
+    return arrayofEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
